617cbe319c2bf7e2e76b2f96f3f43dee23a2f0f9
Rajan Tiwari <rajan@leadschool.in>
2021-05-01 13:43:28 +0530

    extra part

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
-$(document).ready(function(){
 
-    $('.fa-bars').click(function(){
-      $(this).toggleClass('fa-times');
-      $('nav').toggleClass('nav-toggle');
+  $(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
     });
-
-    $('nav ul li a').click(function(){
-      $('.fa-bars').removeClass('fa-times');
-      $('nav').removeClass('nav-toggle');
+    
+    $(window).on('scroll load',function(){
+    
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+    
+        if($(window).scrollTop() > 90){
+            $('header').addClass('header-active');
+        }else{
+            $('header').removeClass('header-active');
+        }
+    
+        $('section').each(function(){
+    
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+    
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+    
+        });
+    
     });
-
-
-    $(window).scroll(function(){
-      if($(window).scrollTop() >= 20){
-        $('header').addClass('active');
-      }else{
-        $('header').removeClass('active');
-      }
+    
     });
-
-  });
